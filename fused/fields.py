1702868b717b582bc72785930be603474e7ba8f8
Riley Banks <waultah@gmail.com>
2015-11-15 10:20:52 +0000

    Reorganized proxy classes

--- fused/fields.py
+++ fused/fields.py
@@ -1,12 +1,9 @@
-from functools import partial
-
-
 class BaseField:
 
     _namesep = ':'
 
     def qualified(self, arg=None):
-        parts = [self.model, self.name]
+        parts = [self.model_name, self.name]
         if arg is not None:
             parts.append(arg)
         return self._namesep.join(parts)
@@ -14,19 +11,65 @@ class BaseField:
 
 class Field(BaseField):
 
-    def __init__(self, unique=False, indexable=False, required=False):
-        if unique:
+    def __init__(self, unique=False, indexable=False, required=False,
+                       auto=False):
+        if unique or auto:
             indexable = True
+
         self.unique = unique
         self.indexable = indexable
         self.required = required
+        self.auto = auto
 
-
-    def __get__(self, this, _type):
+    def __get__(self, this, type):
         if this is None:
             raise TypeError('Expected instance, None found')
-        class attrproxy:
-            def __getattr__(_, name):
-                return partial(getattr(this.redis, name), self.qualified())
-        return attrproxy()
+        key = self.qualified()
+        return self._type(key, this) if self.auto else attrproxy(key, this)
 
+
+# Proxy classes
+
+class callproxy:
+
+    def __init__(self, key, model, attr):
+        self.key, self.model, self.attr = key, model, attr
+
+    def __call__(self, *a, **ka):
+        meth = getattr(self.model.__redis__, self.attr)
+        return meth(self.key, *a, **ka)
+
+
+class attrproxy:
+
+    def __init__(self, key, model):
+        self.key, self.model = key, model
+
+    def __getattr__(self, attr):
+        return callproxy(self.key, self.model, attr)
+
+
+class autotype:
+
+    def __init__(self, key, model):
+        self.key, self.model = key, model
+        super().__init__(self._get())
+
+
+# Types
+
+class Set(autotype, set):
+    def _get(self):
+        return self.model.__redis__.smembers(self.key)
+
+
+class List(autotype, list):
+    def _get(self):
+        return self.model.__redis__.lrange(self.key, 0, -1)
+
+
+class SetField(Field):
+    _type = Set
+
+class ListField(Field):
+    _type = List
--- fused/model.py
+++ fused/model.py
@@ -1,27 +1,31 @@
 from . import fields
+from abc import ABCMeta
 
-class MetaModel(type):
 
-    def __new__(mcs, model, base, namespace):
-        cls = super().__new__(mcs, model, base, namespace)
+class MetaModel(ABCMeta):
+
+    def __new__(mcs, model_name, base, attrs):
+        cls = super().__new__(mcs, model_name, base, attrs)
         cls._fields = {}
         cls._unique_fields = {}
         cls._indexable_fields = {}
         cls._required_fields = {}
 
-        for name, ob in namespace.items():
-            if isinstance(ob, fields.BaseField):
-                ob.name, ob.model = name, model
-                cls._fields[name] = ob
+        field_attrs = ((k, v) for k, v in attrs.items()
+                         if isinstance(v, fields.BaseField))
+        for name, field in field_attrs:
+            field.name, field.model_name = name, model_name
+            cls._fields[name] = field
 
-                if ob.unique:
-                    cls._unique_fields[name] = ob
+            if field.unique:
+                cls._unique_fields[name] = field
 
-                if ob.required:
-                    cls._required_fields[name] = ob
+            if field.required:
+                cls._required_fields[name] = field
 
-                if ob.indexable:
-                    cls._indexable_fields[name] = ob
+            if field.indexable:
+                cls._indexable_fields[name] = field
+        return cls
                     
 
 class BaseModel(metaclass=MetaModel):
@@ -29,6 +33,8 @@ class BaseModel(metaclass=MetaModel):
     def __init__(self, **ka):
         self.__context_depth__ = 0
         self.__redis__ = self.redis
+        for field in self._fields.values():
+            field.model = self
         if ka:
             # Will only search by one pair
             if len(ka) > 1:
@@ -52,3 +58,6 @@ class BaseModel(metaclass=MetaModel):
         if not self.__context_depth__:
             self.redis.execute()
             self.redis = self.__redis__
+
+    def _in_cm(self):
+        return self.__context_depth__ != 0
