f6934953666684f9e86f14215a5097069356e07a
Riley Banks <waultah@gmail.com>
2015-11-21 19:56:39 +0000

    Okay, autotypes aren't going anywhere

--- fused/fields.py
+++ fused/fields.py
@@ -27,10 +27,10 @@ class Field(BaseField):
         key = self.qualified()
         return self._type(key, this) if self.auto else commandproxy(key, this)
 
-    # def __set__(self, this, value):
-    #     if not isinstance(value, autotype):
-    #         raise TypeError('Field.__set__ only works for Auto fields')
-    #     self._type.save(self.qualified(), this, value)
+    def __set__(self, this, value):
+        if not isinstance(value, autotype):
+            raise TypeError('Field.__set__ only works for Auto fields')
+        self._type.save(self.qualified(), this, value)
 
 
 
@@ -63,66 +63,66 @@ class commandproxy:
                     self.key, id(self))
 
 
-# class autotype:
+class autotype:
 
-#     def __init__(self, key, model):
-#         self.key, self.model = key, model
-#         super().__init__(self._get())
-
-
-# # Types
-
-# class _Set(autotype, set):
-#     def _get(self):
-#         return self.model.__redis__.smembers(self.key)
-#     @classmethod
-#     def save(cls, key, model, value):
-#         pass
+    def __init__(self, key, model):
+        self.key, self.model = key, model
+        super().__init__(self._get())
 
 
-# class _List(autotype, list):
-#     def _get(self):
-#         return self.model.__redis__.lrange(self.key, 0, -1)
-#     @classmethod
-#     def save(cls, key, model, value):
-#         pass
+# Types
+
+class _Set(autotype, set):
+    def _get(self):
+        return self.model.__redis__.smembers(self.key)
+    @classmethod
+    def save(cls, key, model, value):
+        pass
 
 
-# class _String(autotype, str):
-#     def _get(self):
-#         return self.model.__redis__.get(self.key)
-#     @classmethod
-#     def save(cls, key, model, value):
-#         pass
+class _List(autotype, list):
+    def _get(self):
+        return self.model.__redis__.lrange(self.key, 0, -1)
+    @classmethod
+    def save(cls, key, model, value):
+        pass
 
 
-# class _Integer(autotype, int):
-#     def _get(self):
-#         return "TODO"
-#     @classmethod
-#     def save(cls, key, model, value):
-#         pass
+class _String(autotype, str):
+    def _get(self):
+        return self.model.__redis__.get(self.key)
+    @classmethod
+    def save(cls, key, model, value):
+        pass
 
 
-# class _Hash(autotype, dict):
-#     def _get(self):
-#         return {}
-#     @classmethod
-#     def save(cls, key, model, value):
-#         pass
+class _Integer(autotype, int):
+    def _get(self):
+        return "TODO"
+    @classmethod
+    def save(cls, key, model, value):
+        pass
 
 
-# class Set(Field):
-#     _type = _Set
+class _Hash(autotype, dict):
+    def _get(self):
+        return {}
+    @classmethod
+    def save(cls, key, model, value):
+        pass
 
-# class List(Field):
-#     _type = _List
 
-# class Integer(Field):
-#     _type = _Integer
+class Set(Field):
+    _type = _Set
 
-# class String(Field):
-#     _type = _String
+class List(Field):
+    _type = _List
 
-# class Hash(Field):
-#     _type = _Hash
+class Integer(Field):
+    _type = _Integer
+
+class String(Field):
+    _type = _String
+
+class Hash(Field):
+    _type = _Hash
