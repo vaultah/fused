a57a3ab7a428533f76db96cdc47b65eb055b02ae
Riley Banks <waultah@gmail.com>
2015-11-25 10:26:26 +0000

    Mark as optimizable

--- fused/utils.py
+++ fused/utils.py
@@ -4,6 +4,7 @@ from contextlib import ExitStack
 
 SCRIPTS = {}
 
+# Optimizable in Python 3.5
 with ExitStack() as es:
     for p in Path('scripts').glob('*.lua'):
         file = es.enter_context(p.open())
